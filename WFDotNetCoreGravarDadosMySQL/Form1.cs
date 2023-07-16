8a4fa2b2dc706c4d26d6e4c688eaa05aaced8e3d
zennom <delimaviviane@yahoo.com.br>
2023-07-16 20:58:13 -0300

    fazendo código para buscar os dados

--- WFDotNetCoreGravarDadosMySQL/Form1.cs
+++ WFDotNetCoreGravarDadosMySQL/Form1.cs
@@ -18,6 +18,16 @@ namespace WFDotNetCoreGravarDadosMySQL
         public Form1()
         {
             InitializeComponent();
+
+            lst_contatos.View = View.Details;
+            lst_contatos.AllowColumnReorder = true;
+            lst_contatos.FullRowSelect = true;
+            lst_contatos.GridLines = true;
+
+            lst_contatos.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            lst_contatos.Columns.Add("Nome", 150, HorizontalAlignment.Left);
+            lst_contatos.Columns.Add("Email", 150, HorizontalAlignment.Left);
+            lst_contatos.Columns.Add("Telefone", 150, HorizontalAlignment.Left);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -50,25 +60,39 @@ namespace WFDotNetCoreGravarDadosMySQL
             }
        
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
            try
             {
-
                 string q = " '%" + txt_buscar.Text + "%' ";
-                //SELECT * FROM CONTATO WHERE NOME %VIVIANE%
 
                 Conexao = new MySqlConnection(data_source);
                 
-                
                 string sql = "SELECT * " + "FROM contato " +
-                    "WHERE nome LIKE "+q+"OR email"+q;
+                    "WHERE nome LIKE "+q+"OR email LIKE "+q;
 
                 MySqlCommand comando = new MySqlCommand(sql, Conexao);
                 Conexao.Open();
-                comando.ExecuteReader();
+                MySqlDataReader reader = comando.ExecuteReader();
 
+                lst_contatos.Items.Clear();
+
+                while(reader.Read())
+                {   
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+
+                    //vamos montar um elemento windowsform que será a nossa linha
+                    var linhaListView = new ListViewItem(row);
+                    /*em seguida pegamos o nosso listview e acrescentamos essa linha
+                    que acabamos de criar */
+                    lst_contatos.Items.Add(linhaListView);
+                }
             }
             catch (Exception ex)
             {
